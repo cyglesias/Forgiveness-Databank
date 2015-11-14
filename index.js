a633b737b8b1a83e7c7efe730fb4d0ef49d5b893
mhellar <mark.hellar@gmail.com>
2015-11-13 17:47:04 -0800

    added DB and Websocket support

---
+++ index.js
@@ -0,0 +1,39 @@
+var levelup = require('levelup');
+var express = require('express');
+var app = express();
+var server = app.listen(3000);
+app.use(express.static('public'));
+
+var io = require('socket.io')(server);
+
+// 1) Create our database, supply location and options. 
+//    This will create or open the underlying LevelDB store. 
+var db = levelup('./mydb')
+var i = 1;
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket) {
+            socket.on('message', function(msg) {
+                // console.log(msg);
+                i = i + 1;
+
+                db.put(i, msg, function(err) {
+                if (err) return console.log('Ooops!', err) // some kind of I/O error 
+                console.log(db.db.getProperty('leveldb.num-files-at-level1'))
+                // 3) fetch by key 
+                db.get(i, function(err, value) {
+                    if (err) return console.log('Ooops!', err) // likely the key was not found 
+                    // ta da! 
+                    io.sockets.emit('data', {
+                        val: value
+                    });
+                    
+                })
+            })
+
+
+            });
+          });
