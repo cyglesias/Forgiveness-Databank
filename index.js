acf7cd4e801476a2c9b8c0376fb78122184929f8
Christina Yglesias <christinayglesias@gmail.com>
2015-11-30 18:21:44 -0800

    styling changes

--- index.js
+++ index.js
@@ -63,6 +63,11 @@ io.on('connection', function(socket) {
             socket.on('apology', function(apologytext) {
                 console.log(apologytext);
                 my_sheet.addRow(1, { apology: apologytext} );
+
+io.on('connection', function(socket) {
+                 socket.on('forgiveness', function(forgivenesstext) {
+                console.log(forgivenesstext);
+                my_sheet.addRow(1, { forgiveness: forgivenesstext} );
         
      // this.getCells = function (opts, cb) {
   //   spreadsheet.getCells( self.id, opts, cb );
